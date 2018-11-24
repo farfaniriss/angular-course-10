e2999ec8fb46650b5132781c48c4ba8f8283a073
farfaniriss <iris.farfan.c@gmail.com>
2018-11-24 00:51:31 -0500

    custom and async validators

--- src/app/components/data-template/data-template.component.ts
+++ src/app/components/data-template/data-template.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
-import { USER } from '../template/template.component';
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Observable } from "rxjs";
+
 
 @Component({
   selector: 'app-data-template',
@@ -16,19 +17,45 @@ export class DataTemplateComponent implements OnInit {
       name : 'Fernando',
       lastName : 'Campero'
     },
-    email : 'fc@holi.com'
+    email : 'fc@holi.com',
+    hobbies: ["Eat","Sleep","Run"]
   } 
 
   constructor() { 
     this.forma = new FormGroup({
-      // 'fullName': new FormGroup({
-      //   'name' : new FormControl('', [Validators.required, Validators.minLength(3)]),
-      //   'lastName' : new FormControl('', Validators.required)
-      // }),
-      'name' : new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'lastName' : new FormControl('', Validators.required),
-      'email' : new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
+      'fullName': new FormGroup({
+        'name' : new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'lastName' : new FormControl('', [Validators.required, this.noHerrera])
+      }),
+      // 'name' : new FormControl('', [Validators.required, Validators.minLength(3)]),
+      // 'lastName' : new FormControl('', Validators.required),
+      'email' : new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
+      'hobbies': new FormArray([
+        new FormControl('Eat', Validators.required)
+      ]),
+      'pass1': new FormControl('', Validators.required),
+      'pass2': new FormControl(),
+      'username': new FormControl('', this.existsUser)
     });
+
+    this.forma.controls['pass2'].setValidators([
+      Validators.required,
+      this.notEqual.bind(this.forma) //for this
+    ])
+
+    this.forma.valueChanges.subscribe( data => {
+      console.log(data);
+    })
+
+    this.forma.controls['username'].valueChanges.subscribe( data => {
+      console.log(data);
+    })
+
+    this.forma.controls['username'].statusChanges.subscribe( data => {
+      console.log(data);
+    })
+
+    //this.forma.setValue( this.user );
   }
 
   ngOnInit() {
@@ -37,6 +64,58 @@ export class DataTemplateComponent implements OnInit {
 
   save(){
     console.log(this.forma.value)
+
+    
+
+    // this.forma.reset({
+    //   fullName: {
+    //     name: "",
+    //     lastName: ""
+    //   },
+    //   email:""
+    // })
   }
 
+  addHobby(){
+    (<FormArray>this.forma.controls['hobbies']).push(
+      new FormControl('Sleep', Validators.required)
+    )
+  }
+
+  //validaciones personalizadas
+
+  noHerrera(control:FormControl) : {[s:string]:boolean}{
+    if (control.value === "herrera"){
+      return {
+        noherrera:true
+      }
+    }
+    return null;
+  }
+
+  notEqual (control: FormControl) : {[s:string]:boolean}{
+
+    let forma:any = this;
+    if (control.value !== forma.controls['pass1'].value){
+      return {
+        notequal:true
+      }
+    }
+    return null;
+  }
+
+  existsUser(control: FormControl): Promise<any> | Observable<any>{
+    let promise = new Promise(
+      ( resolve, reject ) => {
+          setTimeout(() => {
+            if (control.value === "strider") {
+              resolve ({existe:true})
+            } else {
+              resolve (null)
+            }
+          }, 3000);
+      })
+    
+    return promise;
+  }
 }
